1167df5360604ce0ff04e1e043b831f38bc13acb
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-06-19 15:00:50 -0700

    simple liker

--- main.js
+++ main.js
@@ -4,7 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const error = document.querySelector('.hidden');
+const hearts = document.querySelectorAll('.like-glyph');
 
+hearts.forEach(heart => heart.addEventListener("click", servResponse ))
+
+function servResponse(hearts) {
+  mimicServerCall()
+  .then(() => {
+    if(hearts.target.textContent === EMPTY_HEART){
+      hearts.target.textContent = FULL_HEART
+      hearts.target.classList.add('activated-heart')
+    }
+    else if (hearts.target.textContent === FULL_HEART){
+      hearts.target.textContent = EMPTY_HEART
+    }
+  })
+  .catch(() => {
+    error.classList.remove('#hidden')
+    error.textContent = "error occured"
+
+    setTimeout(() => {
+      error.classList = "hidden"}, 3000)
+    })
+  }
 
 
 //------------------------------------------------------------------------------
